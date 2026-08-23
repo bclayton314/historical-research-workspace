613c1d18168eda2e0b1c728de5d8a5390dd19337
Clayton Baker <bclayton314@gmail.com>
2026-08-23 10:35:16 -0500

    Stage_3

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,9 @@
 from app.models.historical_source import HistoricalSource
+from app.models.research_note import ResearchNote
 from app.models.research_project import ResearchProject
 
 __all__ = [
     "HistoricalSource",
+    "ResearchNote",
     "ResearchProject",
 ]
