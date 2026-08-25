ae61023cf510274b3b7e111d4629e2134a9e08c5
Clayton Baker <bclayton314@gmail.com>
2026-08-25 13:29:37 -0500

    Stage_3

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,4 +1,31 @@
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+
+
+@event.listens_for(
+    Engine,
+    "connect",
+)
+def enable_sqlite_foreign_keys(
+    dbapi_connection,
+    connection_record,
+):
+    """Enable foreign-key constraints for SQLite."""
+
+    del connection_record
+
+    cursor = dbapi_connection.cursor()
+
+    try:
+        cursor.execute(
+            "PRAGMA foreign_keys=ON"
+        )
+    except Exception:
+        pass
+    finally:
+        cursor.close()
